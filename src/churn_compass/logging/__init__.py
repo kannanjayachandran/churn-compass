1a979f0cfad4163c17db1b77443e0bdc9e9b7123
kannanjayachandran <kannanjayachandran16@gmail.com>
2025-12-30 19:42:25 +0530

    refactor(logging): remove init file; logger is promoted to global level and is now available as a project root level api

--- src/churn_compass/logging/__init__.py
+++
@@ -1,3 +0,0 @@
-from .logger import setup_logger, log_execution_time, mask_pii, default_logger, set_run_context
-
-__all__ = ["setup_logger", "log_execution_time", "mask_pii", "default_logger", "set_run_context"]
